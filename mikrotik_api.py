f923667b8083649a679b720cb3050242290fdb02
Rei Begaj <rei.begaj02@gmail.com>
2022-06-28 13:57:06 +0200

    Edited file

--- mikrotik_api.py
+++ mikrotik_api.py
@@ -2,12 +2,14 @@ from librouteros import connect
 from getpass import getpass
 import simplejson as json
 
-ip_address = ["192.168.77.1"]
+ip_address = input ("Input router's IP Address:") 
+
+#ip_address = ["192.168.77.1"]
 
 user= input ("Input username: ")
 passw = getpass()
 
 for ip in ip_address:
-    api = connect(username=user, password=passw, host=ip)
-    ip_info = api(cmd="/ip/address/print")
+    api = connect(username=user, password=passw, host=ip_address)
+    ip_info = api(cmd="/export =file=backuppython")
     print (json.dumps(ip_info, indent=3, iterable_as_array=True))
